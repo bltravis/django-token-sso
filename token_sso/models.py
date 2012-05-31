01470d1e6f9322fd692af5702df255e1633a2713
Brooks Travis <brooks.travis@gmail.com>
2012-05-31 14:33:29 -0500

    Tweaks to token model is_valid method, adding a model to track sign-on events for auditing/security purposes, and a model to manage user preferences for sso behavior.

--- token_sso/models.py
+++ token_sso/models.py
@@ -14,17 +14,36 @@ class Token(models.Model):
     site = models.ForeignKey(Site)
     token = models.CharField(max_length=64)
     created = models.DateTimeField(default=datetime.datetime.now))
-    used = models.BooleanField(default=False)
+    used_expired = models.BooleanField(default=False)
     
     def __unicode__(self):
         return 'SSO token for user: %s accessing site: %s' % (self.user, self.site)
     
     def is_valid(self):
-        if self.site == Site.objects.get_current() and self.user.is_active() and not self.used and not datetime.datetime.now() > self.created + datetime.timedelta(seconds=SSO_TOKEN_EXPIRES):
+        if self.site == Site.objects.get_current() and self.user.is_active() and not self.used:
+            if datetime.datetime.now() > self.created + datetime.timedelta(seconds=SSO_TOKEN_EXPIRES):
+                self.used_expired = True
+                self.save()
+                return False
+            self.used_expired = True
             return True
         else:
             return False
 
+class SignOnEvent(models.Model):
+    user = models.ForeignKey(User)
+    site = models.ForeignKey(Site)
+    client_ip = models.IPAddressField()
+    successful = models.BooleanField()
+    event_date_time = models.DateTimeField(default=datetime.datetime.now)
+    
+    def __unicode__(self):
+        if self.successful:
+            return u'Successful SSO Event: %s(user), %s(site), %s(ip)' % (self.user, self.site, self.client_ip)
+        else:
+            return u'Failed SSO Event: %s(user), %s(site), %s(ip)' % (self.user, self.site, self.client_ip)
+    
+    
 class SiteSSOSettings(models.Model):
     """
     Model for storing site-specific token auth consumer url information.
@@ -34,3 +53,17 @@ class SiteSSOSettings(models.Model):
     
     def __unicode__(self):
         return 'Site SSO Settings for %s' % self.site.name
+
+class UserSSOSettings(models.Model):
+    """
+    Model for storing user-specific SSO settings/preferences.
+    """
+    user = models.ForeignKey(User)
+    use_sso_all = models.BooleanField(default=True)
+    sso_sites = models.ManyToManyField(Site)
+    sign_out_all = models.BooleanField(default=True)
+    first_time_prompt = models.BooleanField(default=False)
+    
+    def __unicode__(self):
+        return u'SSO preferences for %s' % (self.user)
+    
