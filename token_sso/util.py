7d46367a5602ed96e999a1854afaff01180c68e7
Brooks Travis <brooks.travis@gmail.com>
2012-05-31 14:34:47 -0500

    Modifying custom secure_required decorator.

--- token_sso/util.py
+++ token_sso/util.py
@@ -1,14 +1,18 @@
 from django.http import HttpResponseRedirect
-def secure_required(view_func):
+def secure_required(reject_insecure=False):
     """Decorator makes sure URL is accessed over https."""
-
-    def _wrapped_view_func(request, *args, **kwargs):
-        if not request.is_secure():
-            if getattr(settings, 'HTTPS_SUPPORT', True):
-                request_url = request.build_absolute_uri(request.get_full_path())
-                secure_url = request_url.replace('http://', 'https://')
-                return HttpResponseRedirect(secure_url)
-        return view_func(request, *args, **kwargs)
+    
+    if reject_insecure and not request.is_secure():#If request is sent over non-secure connection and request data is deemed sufficiently sensitive (login data, for example), reject request.
+        return HttpResponseBadRequest()
+    def _read_opts(func):
+        def _wrapped_view_func(request, *args, **kwargs):
+            if not request.is_secure():
+                if getattr(settings, 'HTTPS_SUPPORT', True):
+                    request_url = request.build_absolute_uri(request.get_full_path())
+                    secure_url = request_url.replace('http://', 'https://')
+                    return HttpResponseRedirect(secure_url)
+            return view_func(request, *args, **kwargs)
+        return _read_opts
     return _wrapped_view_func
 
 
